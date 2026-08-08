bfcbc758c8ae337497407cc5415a206af5d1aadc
kartik12121 <118295876+kartik12121@users.noreply.github.com>
2026-08-08 09:03:20 +0200

    Time: 10 ms (16.42%), Space: 15.6 MB (63.38%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -2,31 +2,21 @@ class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
         priority_queue<pair<int,int>>pq;
-        for(int i=0;i<score.size();i++)
+        int n=score.size();
+        for(int i=0;i<n;i++)
         {
             pq.push({score[i],i});
         }
-        vector<string>result(score.size());
+        vector<string>result(n);
         int rank=1;
         while(!pq.empty())
         {
             auto i=pq.top();
-            if(rank==1)
-            {
-                result[i.second]="Gold Medal";
-            }
-            else if(rank==2)
-            {
-                result[i.second]="Silver Medal";
-            }
-            else if(rank==3)
-            {
-                result[i.second]="Bronze Medal";
-            }
+            if(rank==1)result[i.second]="Gold Medal";
+            else if(rank==2)result[i.second]="Silver Medal";
+            else if(rank==3)result[i.second]="Bronze Medal";
             else
-            {
                 result[i.second]=to_string(rank);
-            }
             rank++;
             pq.pop();
 
