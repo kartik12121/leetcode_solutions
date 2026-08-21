9cb238dc5fff8010acdd1352e3c0374ce92b6fa0
kartik12121 <118295876+kartik12121@users.noreply.github.com>
2026-08-21 17:55:56 +0530

    Time: 2 ms (22.46%), Space: 29.4 MB (93.16%) - LeetHub

---
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        vector<int>arr(encoded.size()+1,0);
+        arr[0]=first;
+        for(int i=0;i<arr.size()-1;i++)
+        {
+            arr[i+1]=encoded[i]^arr[i];
+        }
+        return arr;
+    }
+};
